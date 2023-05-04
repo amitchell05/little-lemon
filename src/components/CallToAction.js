ce6d818fa0e17121f49bb6c641aed222d7b574ca
Angela Mitchell <anglarenmit@gmail.com>
2023-05-03 22:29:33 -0500

    complete layout and styling of homepage

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,26 +1,16 @@
 // Assets
 import restaurantFood from '../assets/restaurantfood.jpg';
 
-// React Tools
-// import useWindowWidth from '../hooks/useWindowWidth';
-
 // Styles
 import './CallToAction.scss';
 
 const CallToAction = () => {
-  // const windowWidth = useWindowWidth();
-
-  // const right = windowWidth - 1001 + 'px';
-
   return (
     <div className='hero'>
       <div className='container'>
         <section>
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
-        </section>
-        <section>
-          <h3 className='visually-hidden'>Hero Image</h3>
           <div className='hero-content'>
             <p className='lead-text'>
               We are a family owned Mediterranean restaurant, focused on
@@ -33,23 +23,6 @@ const CallToAction = () => {
                 className='hero-image'
               />
             </div>
-            {/* {windowWidth >= 1200 && windowWidth < 1292 ? (
-            <div style={{ right: right }}>
-              <img
-                src={restaurantFood}
-                alt='Restaurant food on a serving tray'
-                className='hero-image'
-              />
-            </div>
-          ) : (
-            <div>
-              <img
-                src={restaurantFood}
-                alt='Restaurant food on a serving tray'
-                className='hero-image'
-              />
-            </div>
-          )} */}
           </div>
           <button type='button' className='button button-primary'>
             Reserve a Table
