1b572adb3a66270c132a6cab63e0d8bb6d81266a
Angela Mitchell <anglarenmit@gmail.com>
2023-05-03 21:06:06 -0500

    complete layout for about/chicago component

--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -7,32 +7,34 @@ import './Chicago.scss';
 
 const Chicago = () => {
   return (
-    <article>
+    <article className='about'>
       <h2 className='visually-hidden'>About Little Lemon</h2>
-      <section>
-        <h2>Little Lemon</h2>
-        <h3>Chicago</h3>
-        <p>
-          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-          sint. Velit officia consequat duis enim velit mollit. Exercitation
-          veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
-          ullamco est sit aliqua dolor do amet sint. Velit officia consequat
-          duis enim velit mollit.
-        </p>
-      </section>
-      <section>
-        <h2 className='visually-hidden'>Mario and Adrian</h2>
-        <img
-          src={MarioAndAdrianA}
-          alt='Mario and Adrian'
-          className='about-image'
-        />
-        <img
-          src={MarioAndAdrianB}
-          alt='Mario and Adrian'
-          className='about-image'
-        />
-      </section>
+      <div className='container'>
+        <section className='about-content'>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
+            ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+            duis enim velit mollit.
+          </p>
+        </section>
+        <section className='about-image-group'>
+          <h2 className='visually-hidden'>Mario and Adrian</h2>
+          <img
+            src={MarioAndAdrianA}
+            alt='Mario and Adrian'
+            className='about-image about-image-1'
+          />
+          <img
+            src={MarioAndAdrianB}
+            alt='Mario and Adrian'
+            className='about-image about-image-2'
+          />
+        </section>
+      </div>
     </article>
   );
 };
